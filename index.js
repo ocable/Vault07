f81cbb3a0bc3860c975fdfea6ee60180959a72d5
Orion Cable <noiro.elbac@gmail.com>
2024-01-17 15:05:28 -0500

    added js const and alert

---
+++ index.js
@@ -0,0 +1,7 @@
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+const A = 5*2;
+const B = 80/2;
+const C = 40-1;
+
+alert(message + A + " - " + B  + " - " + C);
